817c54d68e952f97ac6b51d04c06f770dec09262
Ethan <Ethan.Jung@obigo.com>
2025-12-04 09:05:03 +0900

    Add Vercel serverless function handlers

---
+++ api/[...path].js
@@ -0,0 +1,4 @@
+import app from '../server.js';
+
+// Vercel 서버리스 함수로 Express 앱 래핑
+export default app;
--- api/index.js
+++ api/index.js
@@ -1,3 +1,6 @@
 import app from '../server.js';
 
-export default app;
+// Vercel 서버리스 함수 핸들러
+export default async function handler(req, res) {
+  return app(req, res);
+}
